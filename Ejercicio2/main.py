97de0e120be96364b50ab1f870e26a16eec8dbbb
hellboy1511 <sergiodiaz5332@gmail.com>
2022-03-20 18:23:55 +0100

    Changes by alfon

--- Ejercicio2/main.py
+++ Ejercicio2/main.py
@@ -4,16 +4,16 @@ import json
 import numpy as np
 
 con = sqlite3.connect('ejercicio1.db')
+cursorObj = con.cursor()
 
 
 def create():
-    cursorObj = con.cursor()
     cursorObj.execute("DROP TABLE IF EXISTS usuarios")
     cursorObj.execute("DROP TABLE IF EXISTS fechas")
     cursorObj.execute("DROP TABLE IF EXISTS ips")
 
     cursorObj.execute("CREATE TABLE usuarios (username text, telefono integer, contrasena text, provincia text, "
-                      "permisos integer, emails_total integer, emails_phising integer, emails_clicados integer, constraint PK_usuarios primary key (username)) ")
+                      "permisos bool, emails_total integer, emails_phishing integer, emails_clicados integer, constraint PK_usuarios primary key (username)) ")
     cursorObj.execute(
         "CREATE TABLE fechas (username text, fecha text, constraint PK_fechas primary key (username,fecha), constraint FK_fechas_usuarios foreign key (username) references usuarios(username))")
     cursorObj.execute(
@@ -23,7 +23,7 @@ def create():
 
 def instantiate():
     create()
-    cursorObj = con.cursor()
+    sum = 0
     with open("users.json", "r") as file:
         lines = json.load(file)
 
@@ -71,6 +71,73 @@ def instantiate():
                         cursorObj.execute(query)
                         test.append(ip)
     con.commit()
+    return sum
 
+def media_fechas():
+    sum = 0
+    for num in cursorObj.execute("SELECT COUNT(fecha) FROM fechas group by username").fetchall():
+        sum += num[0]
+    return sum / cursorObj.execute("SELECT COUNT(DISTINCT(username)) FROM fechas").fetchone()[0]
+
+def desviacion_fechas():
+    aux = []
+    for num in cursorObj.execute("SELECT COUNT(fecha) FROM fechas group by username").fetchall():
+        aux.append(num[0])
+    return np.std(aux)
+
+def max_fechas():
+    aux = 0
+    for num in cursorObj.execute("SELECT COUNT(fecha) FROM fechas group by username").fetchall():
+        if aux < num[0]:
+            aux = num[0]
+    return aux
+
+def min_fechas():
+    aux = float("inf")
+    for num in cursorObj.execute("SELECT COUNT(fecha) FROM fechas group by username").fetchall():
+        if aux > num[0]:
+            aux = num[0]
+    return aux
+
+
+def media_ips():
+    sum = 0
+    for num in cursorObj.execute("SELECT COUNT(ip) FROM ips group by username").fetchall():
+        sum += num[0]
+    return sum / cursorObj.execute("SELECT COUNT(DISTINCT(username)) FROM ips").fetchone()[0]
+
+def desviacion_ips():
+    aux = []
+    for num in cursorObj.execute("SELECT COUNT(ip) FROM ips group by username").fetchall():
+        aux.append(num[0])
+    return np.std(aux)
+
+def media_emails_totales():
+    sum = 0
+    for num in cursorObj.execute("SELECT emails_total FROM usuarios").fetchall():
+        sum += num[0]
+    return sum / cursorObj.execute("SELECT COUNT(username) FROM usuarios").fetchone()[0]
+
+def desviacion_emails_totales():
+    aux = []
+    for num in cursorObj.execute("SELECT emails_total FROM usuarios").fetchall():
+        aux.append(num[0])
+    return np.std(aux)
+
+def max_emails_totales():
+    return cursorObj.execute("SELECT MAX(emails_total) FROM usuarios").fetchone()[0]
+
+def min_emails_totales():
+    return cursorObj.execute("SELECT MIN(emails_total) FROM usuarios").fetchone()[0]
 
 instantiate()
+print(media_fechas())
+print(desviacion_fechas())
+print(max_fechas())
+print(min_fechas())
+print(media_ips())
+print(desviacion_ips())
+print(media_emails_totales())
+print(desviacion_emails_totales())
+print(max_emails_totales())
+print(min_emails_totales())
