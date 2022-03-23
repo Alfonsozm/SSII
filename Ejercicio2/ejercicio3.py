7010dcfb38ffe8ebff9b19fe8a43526e28c66987
hellboy1511 <sergiodiaz5332@gmail.com>
2022-03-23 22:21:47 +0100

    Ejercicio3 correosmas o correosmenos, según queráis xD

--- Ejercicio2/ejercicio3.py
+++ Ejercicio2/ejercicio3.py
@@ -56,25 +56,25 @@ def varianza(dataframe):
 
 
 def maximo(dataframe) -> int:
-    return dataframe["emails_phising"].max()
+    return dataframe["emails_phishing"].max()
+
+
+def minimo(dataframe) -> int:
+    return dataframe["emails_phishing"].min()
 
 
 #No sé a que se refiere en este apartado
 def valores_missing(dataframe):
     missing = 0
     for index, row in dataframe.iterrows():
-        if row["emails_phishing"] == 0:
+        if row["emails_phishing"] is None:
             missing += 1
     return missing
 
 
 #No sé a que se refiere en este apartado
-def observaciones(dataframe):
-    obs = 0
-    for index, row in dataframe.iterrows():
-        if row["emails_phishing"] != 0:
-            obs += 1
-    return obs
+def observaciones(dataframe) -> int:
+    return dataframe["emails_phishing"].sum()
 
 
 #print(dataframe_permisos(0))
@@ -82,4 +82,48 @@ def observaciones(dataframe):
 #print(dataframe_correos("mayor"))
 #print(dataframe_correos("menor"))
 
-print(varianza(dataframe_correos("mayor")))
+df_administradores = dataframe_permisos(1)
+df_usuarios = dataframe_permisos(0)
+
+print("Resultados por agrupación de permisos:")
+print("Permisos == 1:")
+print("Número de observaciones:", observaciones(df_administradores))
+print("Número de valores ausentes:", valores_missing(df_administradores))
+print("Mediana:", mediana(df_administradores))
+print("Media:", media(df_administradores))
+print("Varianza:", varianza(df_administradores))
+print("Valor máximo:", maximo(df_administradores))
+print("Valor mínimo:", minimo(df_administradores))
+print()
+
+print("Permisos == 0:")
+print("Número de observaciones:", observaciones(df_usuarios))
+print("Número de valores ausentes:", valores_missing(df_usuarios))
+print("Mediana:", mediana(df_usuarios))
+print("Media:", media(df_usuarios))
+print("Varianza:", varianza(df_usuarios))
+print("Valor máximo:", maximo(df_usuarios))
+print("Valor mínimo:", minimo(df_usuarios))
+print()
+
+df_correosmas = dataframe_correos("mayor")
+df_correosmenos = dataframe_correos("menor")
+print("Resultados por agrupación de número de emails recibidos:")
+print("Más de 200 correos:")
+print("Número de observaciones:", observaciones(df_correosmas))
+print("Número de valores ausentes:", valores_missing(df_correosmas))
+print("Mediana:", mediana(df_correosmas))
+print("Media:", media(df_correosmas))
+print("Varianza:", varianza(df_correosmas))
+print("Valor máximo:", maximo(df_correosmas))
+print("Valor mínimo:", minimo(df_correosmas))
+print()
+
+print("Menos de 200 correos:")
+print("Número de observaciones:", observaciones(df_correosmenos))
+print("Número de valores ausentes:", valores_missing(df_correosmenos))
+print("Mediana:", mediana(df_correosmenos))
+print("Media:", media(df_correosmenos))
+print("Varianza:", varianza(df_correosmenos))
+print("Valor máximo:", maximo(df_correosmenos))
+print("Valor mínimo:", minimo(df_correosmenos))
