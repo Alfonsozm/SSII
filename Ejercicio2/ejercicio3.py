f6edfc58653e8fbb41198af2c46cf55909f5d84c
hellboy1511 <sergiodiaz5332@gmail.com>
2022-03-23 16:48:48 +0100

    Ejercicio3 dataframe correos

--- Ejercicio2/ejercicio3.py
+++ Ejercicio2/ejercicio3.py
@@ -4,6 +4,7 @@ import sqlite3
 con = sqlite3.connect('database.db')
 cursorObj = con.cursor()
 
+
 def dataframe_permisos(permisos: int):
     if permisos == 1 or permisos == 0:
         df = pd.read_sql_query("SELECT * FROM usuarios WHERE permisos = \'{}\' group by username".format(permisos), con)
@@ -16,5 +17,32 @@ def dataframe_permisos(permisos: int):
         return "error"
 
 
+def dataframe_correos(rango: str):
+    correos = 200
+
+    if rango == "mayor":
+        df = pd.read_sql_query("SELECT * FROM usuarios WHERE emails_total >= \'{}\' group by username".format(correos),
+                               con)
+        df["IPs"] = pd.read_sql_query(
+            "SELECT COUNT(ip) FROM ips INNER JOIN usuarios USING(username) WHERE emails_total "
+            ">= \'{}\' group by username".format(correos), con)
+        df["Fechas"] = pd.read_sql_query("SELECT COUNT(fecha) FROM fechas INNER JOIN usuarios USING(username) WHERE "
+                                         "emails_total >= \'{}\' group by username".format(correos), con)
+        return df
+    elif rango == "menor":
+        df = pd.read_sql_query("SELECT * FROM usuarios WHERE emails_total <= \'{}\' group by username".format(correos),
+                               con)
+        df["IPs"] = pd.read_sql_query(
+            "SELECT COUNT(ip) FROM ips INNER JOIN usuarios USING(username) WHERE emails_total "
+            "<= \'{}\' group by username".format(correos), con)
+        df["Fechas"] = pd.read_sql_query("SELECT COUNT(fecha) FROM fechas INNER JOIN usuarios USING(username) WHERE "
+                                         "emails_total <= \'{}\' group by username".format(correos), con)
+        return df
+    else:
+        return "error"
+
+
 print(dataframe_permisos(0))
 print(dataframe_permisos(1))
+print(dataframe_correos("mayor"))
+print(dataframe_correos("menor"))
