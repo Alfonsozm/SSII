968f06df1caf527d0b015ae81cb132df94a87cea
hellboy1511 <sergiodiaz5332@gmail.com>
2022-03-29 21:40:59 +0200

    gráficos ejercicio1

--- Practica2/ejercicio1.py
+++ Practica2/ejercicio1.py
@@ -1,35 +1,82 @@
 import pandas as pd
 import sqlite3
-import numpy as np
 import matplotlib.pyplot as plt
-from flask import Flask
+from flask import Flask, render_template
 import io
 from flask import Response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-con = sqlite3.connect('database.db')
+from matplotlib.ticker import MaxNLocator
+
+con = sqlite3.connect('database.db', check_same_thread=False)
 cursorObj = con.cursor()
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
-@app.route('/')
-def hello_world():
-    return '<p>Hello, World!</p>'
+@app.route('/users/graph')
+def users_graph():
+    df = df_critic_users()
+    fig, ax = plt.subplots(figsize=(6, 5))
+    plt.subplots_adjust(bottom=0.3)
+    x = df["username"]
+    y = df["prob_click"]
+    ax.bar(x, y, color='r')
+    plt.xticks(rotation=65)
+    plt.xlabel("Usernames")
+    plt.ylabel("Probability to click on phishing email")
+    plt.title("Top x critic users")
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    plt.close(fig)
+    return Response(output.getvalue(), mimetype='image/png')
 
-
-@app.route('/grafico')
-def usuarios_criticos():
-
-
-    fig = plt.Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = np.random.rand(100)
-    ys = np.random.rand(100)
-    axis.plot(xs, ys)
+@app.route('/webs/graph')
+def webs_graph():
+    df = df_vuln_webs()
+    fig, ax = plt.subplots(figsize=(6, 5))
+    plt.subplots_adjust(bottom=0.4)
+    x = df["url"]
+    y = df["Politicas"]
+    ax.bar(x, y, color='r')
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.xticks(rotation=90)
+    plt.xlabel("url")
+    plt.ylabel("Total de políticas actualizadas")
+    plt.title("Top x vulnerable webs")
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
+@app.route("/users/")
+def users():
+    return render_template('users.html', url='/users/graph')
+
+@app.route("/webs")
+def webs():
+    return render_template('webs.html', url='/webs/graph')
+
+def df_critic_users():
+    df = pd.read_sql_query("SELECT username, emails_clicados, emails_phishing, contrasena FROM usuarios", con)
+    for index, row in df.iterrows():
+        if row["emails_phishing"] != 0:
+            df._set_value(index, "prob_click", row["emails_clicados"] / row["emails_phishing"])
+        else:
+            df._set_value(index, "prob_click", 0)
+    df = df.sort_values("prob_click", ascending=False)
+
+    with open("weak_pass.txt", "r") as file:
+        weak_passwords = set(file.read().split("\n"))
+    df = df[df["contrasena"].isin(weak_passwords)]
+    df = df.head(10)
+    return df
+
+def df_vuln_webs():
+    #Qué se entiende por web vulnerable?
+    df = pd.read_sql_query("SELECT url, cookies, aviso, proteccion_de_datos FROM webs ORDER BY url", con)
+    df["Politicas"] = df["cookies"] + df["aviso"] + df["proteccion_de_datos"]
+    df = df.sort_values("Politicas", ascending=False)
+    #df = df.replace({0:1, 1:0})
+    return df
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
