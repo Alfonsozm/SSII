08fcaf6b1859ce26a31454bc697c15343b8a29e1
hellboy1511 <sergiodiaz5332@gmail.com>
2022-04-04 11:38:06 +0200

    web template

--- Practica2/ejercicio1.py
+++ Practica2/ejercicio1.py
@@ -12,52 +12,21 @@ cursorObj = con.cursor()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-
-@app.route('/users/graph')
-def users_graph():
-    df = df_critic_users(10)
-    fig, ax = plt.subplots(figsize=(6, 5))
-    plt.subplots_adjust(bottom=0.3)
-    x = df["username"]
-    y = df["prob_click"]
-    ax.bar(x, y, color='r')
-    plt.xticks(rotation=65)
-    plt.xlabel("Usernames")
-    plt.ylabel("Probability to click on phishing email")
-    plt.title("Top x critic users")
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    plt.close(fig)
-    return Response(output.getvalue(), mimetype='image/png')
-
-
-@app.route('/webs/graph')
-def webs_graph():
-    df = df_vuln_webs(10)
-    fig, ax = plt.subplots(figsize=(6, 5))
-    plt.subplots_adjust(bottom=0.4)
-    x = df["url"]
-    y = df["Politicas"]
-    ax.bar(x, y, color='r')
-    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-    plt.xticks(rotation=90)
-    plt.xlabel("url")
-    plt.ylabel("Total de políticas actualizadas")
-    plt.title("Top x vulnerable webs")
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    plt.close(fig)
-    return Response(output.getvalue(), mimetype='image/png')
-
+@app.route("/")
+def iindex():
+    return render_template('index.html')
 
 @app.route("/users/")
 def users():
-    return render_template('users.html', url='/users/graph')
+    return render_template('users.html')
 
+@app.route("/pages/")
+def vuln_webs():
+    return render_template('pages.html')
 
-@app.route("/webs")
-def webs():
-    return render_template('webs.html', url='/webs/graph')
+@app.route("/vulnerabilities/")
+def vulns():
+    return render_template('vulnerabilities.html')
 
 
 def df_critic_users(top: int):
@@ -83,6 +52,8 @@ def df_vuln_webs(top: int):
     df = df.sort_values("Politicas").head(top)
     # df = df.replace({0:1, 1:0})
     return df
+
+
 
 
 if __name__ == '__main__':
